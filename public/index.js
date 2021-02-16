2615963593c8336dbb245a7ef7830c84a78f0f0f
Chris Curry <Chamilitary216@hotmail.com>
2021-02-15 23:36:20 -0500

    created function to add notes and added UUID packages

--- public/index.js
+++ public/index.js
@@ -1,8 +1,17 @@
+// Web Framework
 const express = require ("express");
-const app
+const app = express ();
+//Used to work with directories and file paths
+const path = require ("path");
+//port
 const port = 3000;
-
+// UUID is used to make seperate ID's for each note so that they can deleted invidually
+const uuid = require ("uuid");
+//File Server Module
 const fs = require ("fs");
+
+//Sets express app to take Data Parsing
+app.use(express.urlencoded({ extended: true}));
 
 
 //Makes call to home page
@@ -13,4 +22,19 @@ app.get("/", function (req, res) {
 // Makes call to the Notes Page
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//Port Listen
+app.listen (PORT, function (){
+    console.log ("Now listening on port: " + PORT);
+});
+
+// Function for adding notes
+app.post ("/api/notes" , (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const addNotes = req.body;
+    addNotes.id = uuid.v4();
+    notes.push(addNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
